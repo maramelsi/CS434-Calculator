f6da108b8bca172bd99d1a9431853b35e3d2fe0e
jabagh97 <jabagh.anjouk@ozu.edu.tr>
2022-01-05 14:32:45 +0300

    bug FIx

--- src/ListenersSimpleFactory/NumberListener.java
+++ src/ListenersSimpleFactory/NumberListener.java
@@ -6,12 +6,21 @@ import java.awt.event.ActionListener;
 
 public class NumberListener implements ActionListener {
     JTextField textField;
+    boolean isNumber = true;
     public NumberListener(JTextField textfield){
         this.textField = textfield;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
         String digit = e.getActionCommand();
+        if (isNumber) {
             textField.setText(digit);
+            isNumber = false;
+        }
+        else {
+            textField.setText(textField.getText()+digit);
+        }
+
+
     }
 }
